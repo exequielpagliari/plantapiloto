e74e764051dae2caeeeb1e09fc820c963888e164
exepa <exepa@hotmail.com>
2023-05-06 08:56:59 -0300

    Creado Carrusle Sin Bootstrap. Quitado Bootstrap de Home.

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var posc = document.querySelector(".articulo")
+var newPosc = document.querySelector(".slide")
+var cantPosc = 0
+var operacion = 0
+console.log(posc)
+console.log(posc.length)
+console.log(newPosc)
+
+const slidesContainer = document.getElementById(".slide");
+const slide = document.querySelector(".articulo");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  cantPosc++
+
+  refrescar()
+});
+
+prevButton.addEventListener("click", () => {
+  cantPosc--
+  refrescar()
+});
+
+
+function refrescar(){
+
+
+if (cantPosc < 0) {cantPosc = 3} 
+else if (cantPosc > 3) {cantPosc = 0}
+operacion = cantPosc * -25
+newPosc.style.transform = `translateX(${ operacion }%)`
+
+
+}
