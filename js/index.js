0a6c257f92f588d08ead70995bbd9786aeec048c
exepa <exepa@hotmail.com>
2023-05-06 09:12:19 -0300

    Mejorado código de Home. Agregado estilo de Header a VisiónYMisión

--- js/index.js
+++ js/index.js
@@ -1,35 +1,25 @@
-var posc = document.querySelector(".articulo")
-var newPosc = document.querySelector(".slide")
-var cantPosc = 0
-var operacion = 0
-console.log(posc)
-console.log(posc.length)
-console.log(newPosc)
-
-const slidesContainer = document.getElementById(".slide");
-const slide = document.querySelector(".articulo");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
+var posc = document.querySelector(".articulo");
+var newPosc = document.querySelector(".slide");
+var cantPosc = 0;
+var operacion = 0;
+console.log(posc);
+console.log(posc.length);
+console.log(newPosc);
 
 nextButton.addEventListener("click", () => {
-  cantPosc++
-
-  refrescar()
+  cantPosc++;
+  refrescar();
 });
 
 prevButton.addEventListener("click", () => {
-  cantPosc--
-  refrescar()
+  cantPosc--;
+  refrescar();
 });
 
 
 function refrescar(){
-
-
 if (cantPosc < 0) {cantPosc = 3} 
-else if (cantPosc > 3) {cantPosc = 0}
-operacion = cantPosc * -25
-newPosc.style.transform = `translateX(${ operacion }%)`
-
-
+else if (cantPosc > 3) {cantPosc = 0};
+operacion = cantPosc * -25;
+newPosc.style.transform = `translateX(${ operacion }%)`;
 }
