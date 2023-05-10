d50bc6aff4e0e30919c5d854788f6499d5785ed8
exepa <exepa@hotmail.com>
2023-05-10 17:13:08 -0300

    Modificados Header de todos los HTML. Agregado estilo de Footer y Header en CSS separados.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+
+            html: "<h1>Aprendiendo componentes</h1><h2>ESTA GENIAL</h2>",
+
+        }
+    },
+
+
+    components:{
+        "prueba":{
+            template: "<section><ul><li>Soy el Item1</li></ul></section>",
+        }
+    }
+
+
+}).mount("#header")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var prevButton = document.getElementById("slide-arrow-prev");
 var nextButton = document.getElementById("slide-arrow-next");
 var posc = document.querySelector(".articulo");
