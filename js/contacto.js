f75944a76e15addd21c0dcfc79a1aada4f127a35
exepa <exepa@hotmail.com>
2023-05-13 20:28:22 -0300

    Posible funcionamiento de Formulario

--- js/contacto.js
+++ js/contacto.js
@@ -1,50 +1,59 @@
 //Validación del formulario de contacto//
 
-function validarFormulario(event) {
-    event.preventDefault(); // Detener el envío del formulario
-  
-    const fname = document.getElementById("fname");
-    const lname = document.getElementById("lname");
-    const email = document.getElementById("email");
-    const subject = document.getElementById("subject");
-  
-    if (fname.value === "") {
-      alert("Por favor, ingrese su nombre");
+
+
+
+
+
+
+function validarFormulario() {
+  const fname = document.getElementById("finame")
+  const lname = document.getElementById("lname");
+  const email = document.getElementById("email");
+  const subject = document.getElementById("subject");
+  const form = document.getElementById("contact-form")
+  let validData = true
+
+
+  if (fname.value == "") {
+      fname.setAttribute('placeholder', "Por favor, ingrese su nombre");
       fname.focus();
-      return false;
-    }
-  
-    if (lname.value === "") {
-      alert("Por favor, ingrese su apellido");
+      validData = false
+  }
+
+
+
+  if (lname.value == "") {
+      lname.setAttribute('placeholder', "Por favor, ingrese su apellido");
       lname.focus();
-      return false;
-    }
-  
-    if (email.value === "") {
-      alert("Por favor, ingrese su correo electrónico");
+      validData = false
+  }
+
+
+  for (arroba in email){ arroba == `@` } 
+  {
+      email.setAttribute('placeholder', "Por favor, ingrese su email");
       email.focus();
-      return false;
-    } else if (!validarEmail(email.value)) {
-      alert("Por favor, ingrese un correo electrónico válido");
-      email.focus();
-      return false;
-    }
-  
-    if (subject.value === "") {
-      alert("Por favor, ingrese un asunto");
+      validData = true
+  }
+
+
+  if (subject.value == "") {
+      subject.setAttribute('placeholder', "Por favor, ingrese su mensaje");
       subject.focus();
-      return false;
-    }
-  
-    // Si llegamos a este punto, el formulario es válido
-    alert("El formulario se envió correctamente. En breve nos comunicaremos.");
-    return true;
+      validData = false
   }
-  
-  function validarEmail(email) {
-    const expresion = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return expresion.test(email);
-  }
-  
-const formulario = document.getElementById("contact-form");
-formulario.addEventListener("submit", validarFormulario);
+
+  form.setAttribute(`onsubmit`, `true`)
+  form.setAttribute(`action`, `https://formsubmit.co/exepa@hotmail.com`)
+  form.setAttribute(`method`, `post`)
+
+
+}
+
+
+
+
+
+
+
