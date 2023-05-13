ec367321e6c52647b2a29131dc49decef3276d8a
exepa <exepa@hotmail.com>
2023-05-13 10:49:24 -0300

    Modificado Contacto, agregando borde a Formulario

---
+++ js/conApiCalen.js
@@ -0,0 +1,36 @@
+<article>
+          <section id="calenNumber">
+            <h3>2</h3>
+            <h4>Mayo</h4>
+          </section>
+          
+          <section id="calnText">
+            <a href="./docs/aireacion.pdf", target="_blank"><h4>Curso: "Aireación de granos" </h4></a>
+            <p>Inicia el curso a distancia del PRO-GRANOS.</p>
+          </section>
+        
+        </article>
+
+        <article>
+          <section id="calenNumber">
+            <h3>29</h3>
+            <h4>Mayo</h4>
+          </section>
+          
+          <section id="calnText">
+            <a href="./docs/insectos.pdf", target="_blank"><h4>Curso: "Control de Plagas e Inocuidad"</h4></a>
+            <p>Inicia el curso a distancia del PRO-GRANOS.</p>
+          </section>
+        </article>
+
+        <article>
+          <section id="calenNumber">
+            <h3>7</h3>
+            <h4>Junio</h4>
+          </section>
+          <section id="calnText">
+            <a href= "https://www.graintrade.org.au/conferences-events/australian-grain-storage-protection-conference-2023", 
+            target="_blank"><h4>NWPGP 2023</h4></a>
+            <p>Inicia la "Australian Grain Storage & Protection Conference".</p>
+          </section>
+        </article>
