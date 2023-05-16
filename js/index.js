931b9029fe6a6d1722097b1c3e495c52c2fe02c3
exepa <exepa@hotmail.com>
2023-05-16 20:42:47 -0300

    Corregida conexión con VUE a los datos de la API en el Home

--- js/index.js
+++ js/index.js
@@ -36,12 +36,15 @@ newPosc.style.transform = `translateX(${ operacion }%)`;
 const app = Vue.createApp({
   data(){
       return{
-        pais: "PAIS",
+        pais: "",
         localidad: "",
         provincia: "",
         fechaYHora: "",
         fecha:"",
-        hora:""
+        hora:"",
+        dia: "",
+        mes: "",
+        anio:""
 
       }
   },
@@ -63,8 +66,10 @@ se debe crear en data() aquello que va a ser modificado.*/
           this.provincia = data.location.region
           this.fechaYHora = data.location.localtime
           this.fecha= ` ${data.location.localtime.slice(0,11)}`,
-          this.hora= ` ${data.location.localtime.slice(11,16)}`
-
+          this.hora= ` ${data.location.localtime.slice(11,16)}`,
+          this.dia=  `${data.location.localtime.slice(8,10)}`,
+          this.mes= `${data.location.localtime.slice(5,7)}`,
+          this.anio= `${data.location.localtime.slice(0,4)}`
       })
 
       .catch(err => console.log(err))
